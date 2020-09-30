33efbee0675133522573aaaa81fcd2c36f02fa9c
axk4647 <arturokuang@gmail.com>
2020-09-30 16:02:24 -0400

    recycleview is bounded to a viewmodel

--- app/src/main/java/com/example/beatbox/SoundViewModel.kt
+++ app/src/main/java/com/example/beatbox/SoundViewModel.kt
@@ -1,5 +1,11 @@
 package com.example.beatbox
 
 class SoundViewModel {
+    var sound: Sound? = null
+        set(sound) {
+            field = sound
+        }
 
+    var title: String? = null
+        get() = sound?.name
 }
